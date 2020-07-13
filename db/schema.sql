f8c8b30cb58800b2e820982abd623d18073738a7
Jared Porter <jaredp17@me.com>
2020-07-13 02:40:34 -0400

    Update schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,27 @@ CREATE DATABASE employeesDB;
 USE employeesDB;
 
 CREATE TABLE department(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary 
-)
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
