c2ea99c7485ee543209b5ee0aa5fd8f58f0012df
Jared Porter <jaredp17@me.com>
2020-07-14 11:07:25 -0400

    Upated schema/seeds

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,24 @@ CREATE DATABASE employeesDB;
 USE employeesDB;
 
 CREATE TABLE department(
-    id INT AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-    id INT AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary VARCHAR(30) NOT NULL,
+department_id INT NOT NULL,
+FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
-    id INT AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY (id),
+    manager_id INT DEFAULT NULL,
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
